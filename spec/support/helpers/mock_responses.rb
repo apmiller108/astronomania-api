359674637e0c8b862464e707a68d3b31f1a6e6f8
Alex Miller <apmiller108@yahoo.com>
2017-06-24 20:22:00 -0400

    ObjectType spec to ensure proper object creation and update

--- spec/support/helpers/mock_responses.rb
+++ spec/support/helpers/mock_responses.rb
@@ -8,4 +8,14 @@ module MockResponses
     path = %w[spec support helpers api_responses neo nineteen_valid_of_twenty.txt]
     File.open(Rails.root.join(*path), 'rb', &:read)
   end
+
+  def valid_asteroid
+    path = %w[spec support helpers api_responses neo valid_asteroid.txt]
+    File.open(Rails.root.join(*path), 'rb', &:read)
+  end
+
+  def invalid_asteroid
+    path = %w[spec support helpers api_responses neo invalid_asteroid.txt]
+    File.open(Rails.root.join(*path), 'rb', &:read)
+  end
 end
