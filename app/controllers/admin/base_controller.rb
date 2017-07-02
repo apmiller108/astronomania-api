6c6968bbf65788de63927cdf1e4b81499d4c5102
Alex Miller <apmiller108@yahoo.com>
2017-07-02 14:23:49 -0400

    Create action fires the import workers when job type is 'import'

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,8 +1,11 @@
 module Admin
   class BaseController < ApplicationController
+    before_action :require_admin
+
     private
 
     def require_admin
+      # TODO: admin check
       # render_unauthorized unless current_user.admin?
     end
   end
--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -1,6 +1,17 @@
 module Admin
   class JobsController < Admin::BaseController
     def create
+      case job_params[:job_type]
+      when 'import'
+        Importer::WORKERS.each_value(&:perform_async)
+      end
+      head :created
+    end
+
+    private
+
+    def job_params
+      params.require(:job).permit(:job_type)
     end
   end
 end
