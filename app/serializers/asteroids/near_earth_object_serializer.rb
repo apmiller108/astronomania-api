382746e4a1798a1f1b320eeca77c0f36fc95426f
Alex Miller <apmiller108@yahoo.com>
2017-07-09 11:01:06 -0400

    Add attributes to serializer and update the json schema helper

--- app/serializers/asteroids/near_earth_object_serializer.rb
+++ app/serializers/asteroids/near_earth_object_serializer.rb
@@ -2,5 +2,12 @@ module Asteroids
   class NearEarthObjectSerializer < ActiveModel::Serializer
     has_one :orbit, class_name: 'Asteroid::Orbit'
     has_many :close_approaches, class_name: 'Asteroid::CloseApproach'
+
+    attributes :id,
+               :name,
+               :nasa_jpl_url,
+               :absolute_magnitude_h,
+               :estimated_diameter,
+               :is_potentially_hazardous_asteroid
   end
 end
